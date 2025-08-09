5d252d8fd46592e785deba62c07adb27c29ff4c8
Marcus441 <psminchia4153@hotmail.com>
2025-08-09 13:13:04 +1000

    feat(nodejs): added node.js template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./cpp;
         description = "Dev environment for C/C++";
       };
+      node = {
+        path = ./node;
+        description = "Dev environment for node.js";
+      };
       # rust = {
       #   path = ./rust;
       #   description = "Dev environment for rust";
---
+++ node/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Node.js + TypeScript Nix template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      nodejs = pkgs.nodejs_20;
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          nodejs
+          pkgs.nodePackages.npm
+        ];
+
+        shellHook = ''
+          echo "Node.js $(node --version)"
+          echo "TypeScript project environment ready!"
+        '';
+      };
+    });
+}
